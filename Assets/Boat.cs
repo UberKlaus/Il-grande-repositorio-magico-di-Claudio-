b1a1d78571e430549837b58f646bfe5f025d112c
UberKlaus <clazena2@yahoo.it>
2015-03-06 15:52:13 +0100

    Prima Commisione
    La mia prima commisione, da 3 anni in su

---
+++ Assets/Boat.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boat : MonoBehaviour 
+{
+
+	public float Speed;
+	public float TorqueSpeed;
+
+	private float CannonAngle;
+	public float CannonPower;
+	
+	public GameObject CannonBall;
+
+	public Transform FirePoint;
+	public Transform CannonPivot;
+
+	private GameObject LastCannonBall;
+	
+	void Start () 
+	{
+	
+	}
+
+	void FixedUpdate () 
+	{
+		if (Input.GetKey (KeyCode.W))
+		{
+			rigidbody.AddRelativeForce (Vector3.forward * Speed);
+		}
+		if (Input.GetKey (KeyCode.S))
+		{
+			rigidbody.AddRelativeForce (Vector3.forward * -Speed);
+		}
+		//----//
+		if (Input.GetKey (KeyCode.D))
+		{
+			rigidbody.AddRelativeTorque (Vector3.up * TorqueSpeed);
+		}
+		if (Input.GetKey (KeyCode.A))
+		{
+			rigidbody.AddRelativeTorque (Vector3.up * -TorqueSpeed);
+		}
+
+		//CannonPivot.transform.eulerAngles = new Vector3 (CannonAngle, transform.rotation.y, 0f);
+		//CannonPivot.transform.rotation.x = CannonAngle;
+	}
+
+	public void SetAngle (float SettedAngle)
+	{
+		CannonAngle = SettedAngle * -30f;
+	}
+
+	public void SetPower (float SettedPower)
+	{
+		CannonPower = SettedPower * 200000f;
+	}
+
+	public void FireAction ()
+	{
+		LastCannonBall = Instantiate (CannonBall, new Vector3(FirePoint.position.x, FirePoint.position.y, FirePoint.position.z), FirePoint.transform.rotation) as GameObject;
+		LastCannonBall.rigidbody.AddRelativeForce (Vector3.forward * CannonPower);
+	}
+}
---
+++ Assets/ResScript.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResScript : MonoBehaviour 
+{
+	private string[] resolutionStrings;
+
+	public int W = 800;
+	public int H = 600;
+
+	private int i = 0;
+
+	void Awake ()
+	{
+		DontDestroyOnLoad(transform.gameObject);
+	}
+
+	void Start () 
+	{
+		Resolution[] resolutions = Screen.resolutions;
+		foreach (Resolution res in resolutions) 
+		{
+			//Debug.Log (res.width + "x" + res.height);
+		}
+
+
+	}	
+	void Update () 
+	{
+	
+	}
+
+	public void ChangeSliderRes (float SlidRes)
+	{
+		if (SlidRes == 0)
+		{
+			W = 800;
+			H = 600;
+		}
+		if (SlidRes == 1)
+		{
+			W = 1024;
+			H = 768;
+		}
+		if (SlidRes == 2)
+		{
+			W = 1152;
+			H = 864;
+		}
+		if (SlidRes == 3)
+		{
+			W = 1280;
+			H = 960;
+		}
+		if (SlidRes == 4)
+		{
+			W = 1360;
+			H = 768;
+		}
+		if (SlidRes == 5)
+		{
+			W = 1920;
+			H = 1200;
+		}
+	}
+
+	public void ChangeRes ()
+	{
+		Screen.SetResolution (W, H, true);
+	}
+}
